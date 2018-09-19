1532f19f4b67a3121aaac527e951657f4d163d58
Godskid89 <37074399+Godskid89@users.noreply.github.com>
2018-09-19 17:54:58 +0100

    Add files via upload

---
+++ Latitude_Longitude.R
@@ -0,0 +1,183 @@
+lagos_boundary <- read.csv("delineation.csv", stringsAsFactors = FALSE)
+
+
+longitude1 <- lagos_boundary$gps.1.._point_gps_longitude[1:6]
+longitude2 <- lagos_boundary$gps.2.._point_gps_longitude[1:6]
+longitude3 <- lagos_boundary$gps.3.._point_gps_longitude[1:6]
+longitude4 <- lagos_boundary$gps.4.._point_gps_longitude[1:6]
+longitude5 <- lagos_boundary$gps.5.._point_gps_longitude[1:6]
+longitude6 <- lagos_boundary$gps.6.._point_gps_longitude[1:6]
+longitude7 <- lagos_boundary$gps.7.._point_gps_longitude[1:6]
+longitude8 <- lagos_boundary$gps.8.._point_gps_longitude[1:6]
+longitude9 <- lagos_boundary$gps.9.._point_gps_longitude[1:6]
+longitude10 <- lagos_boundary$gps.10.._point_gps_longitude[1:6]
+longitude11 <- lagos_boundary$gps.11.._point_gps_longitude[1:6]
+longitude12 <- lagos_boundary$gps.12.._point_gps_longitude[1:6]
+longitude13 <- lagos_boundary$gps.13.._point_gps_longitude[1:6]
+longitude14 <- lagos_boundary$gps.14.._point_gps_longitude[1:6]
+longitude15 <- lagos_boundary$gps.15.._point_gps_longitude[1:6]
+longitude16 <- lagos_boundary$gps.16.._point_gps_longitude[1:6]
+longitude17 <- lagos_boundary$gps.17.._point_gps_longitude[1:6]
+longitude18 <- lagos_boundary$gps.18.._point_gps_longitude[1:6]
+longitude19 <- lagos_boundary$gps.19.._point_gps_longitude[1:6]
+longitude20 <- lagos_boundary$gps.20.._point_gps_longitude[1:6]
+longitude21 <- lagos_boundary$gps.21.._point_gps_longitude[1:6]
+longitude22 <- lagos_boundary$gps.22.._point_gps_longitude[1:6]
+longitude23 <- lagos_boundary$gps.23.._point_gps_longitude[1:6]
+longitude24 <- lagos_boundary$gps.24.._point_gps_longitude[1:6]
+longitude25 <- lagos_boundary$gps.25.._point_gps_longitude[1:6]
+longitude26 <- lagos_boundary$gps.26.._point_gps_longitude[1:6]
+longitude27 <- lagos_boundary$gps.27.._point_gps_longitude[1:6]
+longitude28 <- lagos_boundary$gps.28.._point_gps_longitude[1:6]
+longitude29 <- lagos_boundary$gps.29.._point_gps_longitude[1:6]
+longitude30 <- lagos_boundary$gps.30.._point_gps_longitude[1:6]
+longitude31 <- lagos_boundary$gps.31.._point_gps_longitude[1:6]
+longitude32 <- lagos_boundary$gps.32.._point_gps_longitude[1:6]
+longitude33 <- lagos_boundary$gps.33.._point_gps_longitude[1:6]
+longitude34 <- lagos_boundary$gps.34.._point_gps_longitude[1:6]
+longitude35 <- lagos_boundary$gps.35.._point_gps_longitude[1:6]
+longitude36 <- lagos_boundary$gps.36.._point_gps_longitude[1:6]
+longitude37 <- lagos_boundary$gps.37.._point_gps_longitude[1:6]
+longitude38 <- lagos_boundary$gps.38.._point_gps_longitude[1:6]
+longitude39 <- lagos_boundary$gps.39.._point_gps_longitude[1:6]
+longitude40 <- lagos_boundary$gps.40.._point_gps_longitude[1:6]
+longitude41 <- lagos_boundary$gps.41.._point_gps_longitude[1:6]
+longitude42 <- lagos_boundary$gps.42.._point_gps_longitude[1:6]
+longitude43 <- lagos_boundary$gps.43.._point_gps_longitude[1:6]
+longitude44 <- lagos_boundary$gps.44.._point_gps_longitude[1:6]
+longitude45 <- lagos_boundary$gps.45.._point_gps_longitude[1:6]
+longitude46 <- lagos_boundary$gps.46.._point_gps_longitude[1:6]
+longitude47 <- lagos_boundary$gps.47.._point_gps_longitude[1:6]
+longitude48 <- lagos_boundary$gps.48.._point_gps_longitude[1:6]
+longitude49 <- lagos_boundary$gps.49.._point_gps_longitude[1:6]
+longitude50 <- lagos_boundary$gps.50.._point_gps_longitude[1:6]
+longitude51 <- lagos_boundary$gps.51.._point_gps_longitude[1:6]
+longitude52 <- lagos_boundary$gps.52.._point_gps_longitude[1:6]
+longitude53 <- lagos_boundary$gps.53.._point_gps_longitude[1:6]
+longitude54 <- lagos_boundary$gps.54.._point_gps_longitude[1:6]
+longitude55 <- lagos_boundary$gps.55.._point_gps_longitude[1:6]
+longitude56 <- lagos_boundary$gps.56.._point_gps_longitude[1:6]
+longitude57 <- lagos_boundary$gps.57.._point_gps_longitude[1:6]
+longitude58 <- lagos_boundary$gps.58.._point_gps_longitude[1:6]
+longitude59 <- lagos_boundary$gps.59.._point_gps_longitude[1:6]
+longitude60 <- lagos_boundary$gps.60.._point_gps_longitude[1:6]
+longitude61 <- lagos_boundary$gps.61.._point_gps_longitude[1:6]
+longitude62 <- lagos_boundary$gps.62.._point_gps_longitude[1:6]
+longitude63 <- lagos_boundary$gps.63.._point_gps_longitude[1:6]
+longitude64 <- lagos_boundary$gps.64.._point_gps_longitude[1:6]
+longitude65 <- lagos_boundary$gps.65.._point_gps_longitude[1:6]
+longitude66 <- lagos_boundary$gps.66.._point_gps_longitude[1:6]
+longitude67 <- lagos_boundary$gps.67.._point_gps_longitude[1:6]
+longitude68 <- lagos_boundary$gps.68.._point_gps_longitude[1:6]
+longitude69 <- lagos_boundary$gps.69.._point_gps_longitude[1:6]
+longitude70 <- lagos_boundary$gps.70.._point_gps_longitude[1:6]
+longitude71 <- lagos_boundary$gps.71.._point_gps_longitude[1:6]
+longitude72 <- lagos_boundary$gps.72.._point_gps_longitude[1:6]
+longitude72
+
+
+longitude <- c(longitude1,longitude2,longitude3,longitude4,longitude5,longitude6,longitude7,longitude8,longitude9, longitude10,
+               longitude11,longitude12,longitude13,longitude14,longitude15,longitude16,longitude17,longitude18,longitude19, longitude20,
+               longitude21,longitude22,longitude23,longitude24,longitude25,longitude26,longitude27,longitude28,longitude29, longitude30,
+               longitude31,longitude32,longitude33,longitude34,longitude35,longitude36,longitude37,longitude38,longitude39, longitude40,
+               longitude41,longitude42,longitude43,longitude44,longitude45,longitude46,longitude47,longitude48,longitude49, longitude50,
+               longitude51,longitude52,longitude53,longitude54,longitude55,longitude56,longitude57,longitude58,longitude59, longitude60,
+               longitude61,longitude62,longitude63,longitude64,longitude65,longitude66,longitude67,longitude68,longitude69, longitude70,
+               longitude71,longitude72)
+
+longitude
+
+latitude1 <- lagos_boundary$gps.1.._point_gps_latitude[1:6]
+latitude2 <- lagos_boundary$gps.2.._point_gps_latitude[1:6]
+latitude3 <- lagos_boundary$gps.3.._point_gps_latitude[1:6]
+latitude4 <- lagos_boundary$gps.4.._point_gps_latitude[1:6]
+latitude5 <- lagos_boundary$gps.5.._point_gps_latitude[1:6]
+latitude6 <- lagos_boundary$gps.6.._point_gps_latitude[1:6]
+latitude7 <- lagos_boundary$gps.7.._point_gps_latitude[1:6]
+latitude8 <- lagos_boundary$gps.8.._point_gps_latitude[1:6]
+latitude9 <- lagos_boundary$gps.9.._point_gps_latitude[1:6]
+latitude10 <- lagos_boundary$gps.10.._point_gps_latitude[1:6]
+latitude11 <- lagos_boundary$gps.11.._point_gps_latitude[1:6]
+latitude12 <- lagos_boundary$gps.12.._point_gps_latitude[1:6]
+latitude13 <- lagos_boundary$gps.13.._point_gps_latitude[1:6]
+latitude14 <- lagos_boundary$gps.14.._point_gps_latitude[1:6]
+latitude15 <- lagos_boundary$gps.15.._point_gps_latitude[1:6]
+latitude16 <- lagos_boundary$gps.16.._point_gps_latitude[1:6]
+latitude17 <- lagos_boundary$gps.17.._point_gps_latitude[1:6]
+latitude18 <- lagos_boundary$gps.18.._point_gps_latitude[1:6]
+latitude19 <- lagos_boundary$gps.19.._point_gps_latitude[1:6]
+latitude20 <- lagos_boundary$gps.20.._point_gps_latitude[1:6]
+latitude21 <- lagos_boundary$gps.21.._point_gps_latitude[1:6]
+latitude22 <- lagos_boundary$gps.22.._point_gps_latitude[1:6]
+latitude23 <- lagos_boundary$gps.23.._point_gps_latitude[1:6]
+latitude24 <- lagos_boundary$gps.24.._point_gps_latitude[1:6]
+latitude25 <- lagos_boundary$gps.25.._point_gps_latitude[1:6]
+latitude26 <- lagos_boundary$gps.26.._point_gps_latitude[1:6]
+latitude27 <- lagos_boundary$gps.27.._point_gps_latitude[1:6]
+latitude28 <- lagos_boundary$gps.28.._point_gps_latitude[1:6]
+latitude29 <- lagos_boundary$gps.29.._point_gps_latitude[1:6]
+latitude30 <- lagos_boundary$gps.30.._point_gps_latitude[1:6]
+latitude31 <- lagos_boundary$gps.31.._point_gps_latitude[1:6]
+latitude32 <- lagos_boundary$gps.32.._point_gps_latitude[1:6]
+latitude33 <- lagos_boundary$gps.33.._point_gps_latitude[1:6]
+latitude34 <- lagos_boundary$gps.34.._point_gps_latitude[1:6]
+latitude35 <- lagos_boundary$gps.35.._point_gps_latitude[1:6]
+latitude36 <- lagos_boundary$gps.36.._point_gps_latitude[1:6]
+latitude37 <- lagos_boundary$gps.37.._point_gps_latitude[1:6]
+latitude38 <- lagos_boundary$gps.38.._point_gps_latitude[1:6]
+latitude39 <- lagos_boundary$gps.39.._point_gps_latitude[1:6]
+latitude40 <- lagos_boundary$gps.40.._point_gps_latitude[1:6]
+latitude41 <- lagos_boundary$gps.41.._point_gps_latitude[1:6]
+latitude42 <- lagos_boundary$gps.42.._point_gps_latitude[1:6]
+latitude43 <- lagos_boundary$gps.43.._point_gps_latitude[1:6]
+latitude44 <- lagos_boundary$gps.44.._point_gps_latitude[1:6]
+latitude45 <- lagos_boundary$gps.45.._point_gps_latitude[1:6]
+latitude46 <- lagos_boundary$gps.46.._point_gps_latitude[1:6]
+latitude47 <- lagos_boundary$gps.47.._point_gps_latitude[1:6]
+latitude48 <- lagos_boundary$gps.48.._point_gps_latitude[1:6]
+latitude49 <- lagos_boundary$gps.49.._point_gps_latitude[1:6]
+latitude50 <- lagos_boundary$gps.50.._point_gps_latitude[1:6]
+latitude51 <- lagos_boundary$gps.51.._point_gps_latitude[1:6]
+latitude52 <- lagos_boundary$gps.52.._point_gps_latitude[1:6]
+latitude53 <- lagos_boundary$gps.53.._point_gps_latitude[1:6]
+latitude54 <- lagos_boundary$gps.54.._point_gps_latitude[1:6]
+latitude55 <- lagos_boundary$gps.55.._point_gps_latitude[1:6]
+latitude56 <- lagos_boundary$gps.56.._point_gps_latitude[1:6]
+latitude57 <- lagos_boundary$gps.57.._point_gps_latitude[1:6]
+latitude58 <- lagos_boundary$gps.58.._point_gps_latitude[1:6]
+latitude59 <- lagos_boundary$gps.59.._point_gps_latitude[1:6]
+latitude60 <- lagos_boundary$gps.60.._point_gps_latitude[1:6]
+latitude61 <- lagos_boundary$gps.61.._point_gps_latitude[1:6]
+latitude62 <- lagos_boundary$gps.62.._point_gps_latitude[1:6]
+latitude63 <- lagos_boundary$gps.63.._point_gps_latitude[1:6]
+latitude64 <- lagos_boundary$gps.64.._point_gps_latitude[1:6]
+latitude65 <- lagos_boundary$gps.65.._point_gps_latitude[1:6]
+latitude66 <- lagos_boundary$gps.66.._point_gps_latitude[1:6]
+latitude67 <- lagos_boundary$gps.67.._point_gps_latitude[1:6]
+latitude68 <- lagos_boundary$gps.68.._point_gps_latitude[1:6]
+latitude69 <- lagos_boundary$gps.69.._point_gps_latitude[1:6]
+latitude70 <- lagos_boundary$gps.70.._point_gps_latitude[1:6]
+latitude71 <- lagos_boundary$gps.71.._point_gps_latitude[1:6]
+latitude72 <- lagos_boundary$gps.72.._point_gps_latitude[1:6]
+
+
+latitude <- c(latitude1,latitude2,latitude3,latitude4,latitude5,latitude6, latitude7, latitude8, latitude9, latitude10,
+              latitude11,latitude12,latitude13,latitude14,latitude15,latitude16, latitude17, latitude18, latitude19, latitude20,
+              latitude21,latitude22,latitude23,latitude24,latitude25,latitude26, latitude27, latitude28, latitude29, latitude30,
+              latitude31,latitude32,latitude33,latitude34,latitude35,latitude36, latitude37, latitude38, latitude39, latitude40,
+              latitude41,latitude42,latitude43,latitude44,latitude45,latitude46, latitude47, latitude48, latitude49, latitude50,
+              latitude51,latitude52,latitude53,latitude54,latitude55,latitude56, latitude57, latitude58, latitude59, latitude60,
+              latitude61,latitude62,latitude63,latitude64,latitude65,latitude66, latitude67, latitude68, latitude69, latitude70,
+              latitude71,latitude72)
+latitude
+
+
+lagostable <- data.frame(longitude,latitude)
+lagostable
+
+write.csv(lagostable, file = "Output.csv")
+
+
+
+
+
